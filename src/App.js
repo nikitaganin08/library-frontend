eea294f7d9b51c56b134f95205f4d9d92600c1f3
ganin.n.a <nikitaganin>
2022-01-15 23:01:12 +0300

    8.18 Log in(add login form)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { useApolloClient } from '@apollo/client'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
     const [page, setPage] = useState('authors')
+    const [token, setToken] = useState(null)
+    const client = useApolloClient()
+
+    useEffect(() => {
+        const token = window.localStorage.getItem('library-user-token')
+        if (token) {
+            setToken(token)
+        }
+    }, [])
+
+    const logout = () => {
+        setToken(null)
+        localStorage.clear()
+        client.resetStore()
+    }
 
     return (
         <div>
             <div>
                 <button onClick={() => setPage('authors')}>authors</button>
                 <button onClick={() => setPage('books')}>books</button>
-                <button onClick={() => setPage('add')}>add book</button>
+                {token ? <button onClick={() => setPage('add')}>add book</button> : null}
+                {token ? <button onClick={() => logout()}>logout</button>
+                    : <button onClick={() => setPage('login')}>login</button>}
             </div>
 
             <Authors
@@ -24,6 +43,11 @@ const App = () => {
 
             <NewBook
                 show={page === 'add'}
+            />
+
+            <LoginForm
+                show={page === 'login'}
+                setToken={setToken}
             />
 
         </div>
--- src/queries.js
+++ src/queries.js
@@ -28,7 +28,9 @@ export const ADD_BOOK = gql`
     mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]! ) {
         addBook(title: $title, author: $author, published: $published, genres: $genres) {
             title,
-            author,
+            author {
+                name
+            },
             published,
             genres,
             id
@@ -43,6 +45,14 @@ export const EDIT_AUTHOR = gql`
             born,
             bookCount,
             id
+        }
+    }
+`
+
+export const LOGIN = gql`
+    mutation login($username: String!, $password: String!) {
+        login(username: $username, password: $password) {
+            value
         }
     }
 `
